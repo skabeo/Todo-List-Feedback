d18d2aff92cc8c5e79a054c5fe10161aa6d5c04e
skabeo <spencerokyere95@gmail.com>
2023-04-04 09:45:20 +0100

    Fix issues

--- index.js
+++ index.js
@@ -1,76 +1,3 @@
-
-
-// function saveTasks() {
-//   localStorage.setItem('tasks', JSON.stringify(tasks));
-// }
-
-// function addTask(description) {
-//   const newTask = { description, completed: false, index: tasks.length };
-//   tasks.push(newTask);
-//   saveTasks();
-// }
-
-// function deleteTask(index) {
-//   tasks.splice(index, 1);
-//   tasks.forEach((task, i) => task.index = i);
-//   saveTasks();
-// }
-
-// function editTask(index, newDescription) {
-//   tasks[index].description = newDescription;
-//   saveTasks();
-// }
-
-// function populateTodoList() {
-//   const todoList = document.getElementById('todo-list');
-//   todoList.innerHTML = '';
-
-//   tasks.forEach((task) => {
-//     const listItem = document.createElement('div');
-//     listItem.classList.add('list-container');
-//     const label = document.createElement('label');
-//     const checkbox = document.createElement('input');
-//     checkbox.type = 'checkbox';
-//     checkbox.checked = task.completed;
-//     checkbox.addEventListener('change', () => {
-//       task.completed = !task.completed;
-//       populateTodoList();
-//     });
-//     label.appendChild(checkbox);
-//     label.appendChild(document.createTextNode(task.description));
-
-//     const img = document.createElement('img');
-//     img.src = 'img/menu.png';
-//     img.alt = 'menu';
-//     img.classList.add('menu');
-
-//     const todo = document.querySelector('#add-list').value;
-//     addTask(todo);
-
-//     const imgContainer = document.createElement('div');
-//     imgContainer.classList.add('list-alignment');
-
-//     imgContainer.appendChild(label);
-//     imgContainer.appendChild(img);
-
-//     const hr = document.createElement('hr');
-//     hr.classList.add('list-line');
-//     listItem.appendChild(imgContainer);
-//     listItem.appendChild(hr);
-
-//     todoList.appendChild(listItem);
-//   });
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   populateTodoList();
-// });
-
-// const arrow = document.querySelector('#left-arrow');
-// arrow.classList.add('left-arrow');
-// arrow.src = 'img/left.png';
-
-
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [
   { description: 'wash the dishes', completed: false, index: 0 },
   { description: 'complete To Do list project', completed: false, index: 1 },
@@ -183,9 +110,7 @@ function deleteTask(index) {
 }
 
 function editTask(index, newDescription) {
-  console.log(`Updating task at index ${index} with new description: ${newDescription}`);
   tasks[index].description = newDescription;
-  // taskDescription.textContent = newDescription;
   saveTasks();
 }
 
